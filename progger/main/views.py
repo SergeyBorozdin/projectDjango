c478e0198762db1cb39452a595a27e0af00cbc45
Sergey Borozdin <borozdinsm@yandex.ru>
2024-06-14 13:59:30 +0300

    added tempates hhtml in main

--- progger/main/views.py
+++ progger/main/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h4> test work <h4>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('<h2> about test work <h2>')
+    return render(request, 'main/about.html')
 
-
-def register(request):
-    return HttpResponse('<h2> register test work <h2>')
