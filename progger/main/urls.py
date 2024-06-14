4fcb6e113eb7c215fa1ef5b62bd36bafe3404e6a
Sergey Borozdin <borozdinsm@yandex.ru>
2024-06-14 22:59:57 +0300

    change all design

--- progger/main/urls.py
+++ progger/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('add_register', views.add_register),
-    path('delete_register', views.delete_register),
+    path('', views.index, name='register'),
+    path('about', views.about, name='about'),
+    path('add_register', views.add_register, name='add_register'),
+    path('delete_register', views.delete_register, name='delete_register'),
 ]
