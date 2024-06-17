9e90a91f94a280bbeecdc29f5be9064a6a3cf6db
Sergey Borozdin <borozdinsm@yandex.ru>
2024-06-17 20:10:33 +0300

    added class Equipment and superuser

--- progger/main/models.py
+++ progger/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Equipment(models.Model):
+    article = models.CharField('Артикул поставщика', max_length=25)
+    lm_article = models.CharField('Артикул ЛМ', max_length=25)
+    name = models.CharField('Наименование', max_length=250)
+    length = models.IntegerField('Длина')
+    width = models.IntegerField('Ширина')
+    depth = models.IntegerField('Высота')
+    shop = models.CharField('Магазин', max_length=50)
+    amount = models.IntegerField('Количество')
+    address = models.CharField('Адрес хранения', max_length=25)
+    image = models.ImageField(upload_to='equipment_photos/', blank=True, null=True)
+    date = models.DateField('Дата')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Запись в реестр'
+        verbose_name_plural = 'Реестр оборудования'
+
+
+    @property
+    def has_photo(self):
+        return bool(self.photo and hasattr(self.photo, 'url'))
--- progger/progger/settings.py
+++ progger/progger/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-1j+o(5dvbf36ae8_ejp(s=%$o$#*d$qc!ikhfdq7_gwc80z)8^
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'progger.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,18 +96,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
