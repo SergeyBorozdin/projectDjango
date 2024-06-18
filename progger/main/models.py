6ede2659ef94b02bac1ef8156ed2816d71ffa2ac
Sergey Borozdin <borozdinsm@yandex.ru>
2024-06-18 22:58:25 +0300

    added superuser and model equipment

--- progger/main/models.py
+++ progger/main/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Equipment(models.Model):
-    article = models.CharField('Артикул поставщика', max_length=25)
-    lm_article = models.CharField('Артикул ЛМ', max_length=25)
+    title = models.CharField('title', max_length=250)
+    article = models.CharField('Артикул поставщика', max_length=250)
+    lm_article = models.CharField('Артикул ЛМ', max_length=250)
     name = models.CharField('Наименование', max_length=250)
     length = models.IntegerField('Длина')
     width = models.IntegerField('Ширина')
     depth = models.IntegerField('Высота')
-    shop = models.CharField('Магазин', max_length=50)
+    shop = models.CharField('Магазин', max_length=500)
     amount = models.IntegerField('Количество')
-    address = models.CharField('Адрес хранения', max_length=25)
+    address = models.CharField('Адрес хранения', max_length=250)
     image = models.ImageField(upload_to='equipment_photos/', blank=True, null=True)
     date = models.DateField('Дата')
 
@@ -21,8 +22,3 @@ class Equipment(models.Model):
     class Meta:
         verbose_name = 'Запись в реестр'
         verbose_name_plural = 'Реестр оборудования'
-
-
-    @property
-    def has_photo(self):
-        return bool(self.photo and hasattr(self.photo, 'url'))
